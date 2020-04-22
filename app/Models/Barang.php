17ef9fe1e1218be3ccbfc4e39c6d4a47a3d48190
farhan <farhandy99@gmail.com>
2020-04-22 10:55:53 +0700

    master barang

---
+++ app/Models/Barang.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Barang extends Model
+{
+    protected $table = 'barang';
+    public $timestamps = false; 
+    protected $fillable = ['nama_barang', 'deskripsi', 'harga', 'stok', 'gambar'];
+}
---
+++ database/migrations/2020_04_21_125725_create_barang_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBarangTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('barang', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nama_barang', 255);
+            $table->string('deskripsi', 255);
+            $table->integer('harga');
+            $table->integer('stok');
+            $table->binary('gambar');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('barang');
+    }
+}
