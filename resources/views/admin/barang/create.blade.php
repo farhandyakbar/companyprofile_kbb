860cca1985c320ee98791219c1a5d8faee8a25e8
farhan <farhandy99@gmail.com>
2020-05-19 20:45:26 +0700

    form barang

--- resources/views/admin/barang/create.blade.php
+++ resources/views/admin/barang/create.blade.php
@@ -7,58 +7,38 @@
 <div class="row">
     <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
         <div class="section-block" id="basicform">
-            <h3 class="section-title">Basic Form Elements</h3>
-            <p>Use custom button styles for actions in forms, dialogs, and more with support for multiple sizes, states,
-                and more.</p>
+            <h3 class="section-title">Tambah Data Barang</h3>
         </div>
         <div class="card">
-            <h5 class="card-header">Basic Form</h5>
             <div class="card-body">
                 <form>
                     <div class="form-group">
-                        <label for="inputText3" class="col-form-label">Input Text</label>
-                        <input id="inputText3" type="text" class="form-control">
+                        <label class="col-form-label">Nama Barang</label>
+                        <input id="nama" type="text" class="form-control">
                     </div>
                     <div class="form-group">
-                        <label for="inputEmail">Email address</label>
-                        <input id="inputEmail" type="email" placeholder="name@example.com" class="form-control">
-                        <p>We'll never share your email with anyone else.</p>
+                        <label for="input-select">Kategori Barang</label>
+                        <select class="form-control" id="kategori">
+                            <option>Choose Example</option>
+                        </select>
                     </div>
                     <div class="form-group">
-                        <label for="inputText4" class="col-form-label">Number Input</label>
-                        <input id="inputText4" type="number" class="form-control" placeholder="Numbers">
+                        <label for="exampleFormControlTextarea1">Deskripsi</label>
+                        <textarea class="form-control" id="deskripsi" rows="3"></textarea>
                     </div>
                     <div class="form-group">
-                        <label for="inputPassword">Password</label>
-                        <input id="inputPassword" type="password" placeholder="Password" class="form-control">
+                        <label for="inputText4" class="col-form-label">Harga</label>
+                        <input id="harga" type="number" class="form-control" placeholder="Harga">
+                    </div>
+                    <div class="form-group">
+                        <label for="inputText4" class="col-form-label">Stok</label>
+                        <input id="stok" type="number" class="form-control" placeholder="Stok">
                     </div>
                     <div class="custom-file mb-3">
                         <input type="file" class="custom-file-input" id="customFile">
-                        <label class="custom-file-label" for="customFile">File Input</label>
+                        <label class="custom-file-label" for="customFile">Gambar</label>
                     </div>
-                    <div class="form-group">
-                        <label for="exampleFormControlTextarea1">Example textarea</label>
-                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                    </div>
-                </form>
-            </div>
-            <div class="card-body border-top">
-                <h3>Sizing</h3>
-                <form>
-                    <div class="form-group">
-                        <label for="inputSmall" class="col-form-label">Small</label>
-                        <input id="inputSmall" type="text" value=".form-control-sm"
-                            class="form-control form-control-sm">
-                    </div>
-                    <div class="form-group">
-                        <label for="inputDefault" class="col-form-label">Default</label>
-                        <input id="inputDefault" type="text" value="Default input" class="form-control">
-                    </div>
-                    <div class="form-group">
-                        <label for="inputLarge" class="col-form-label">Large</label>
-                        <input id="inputLarge" type="text" value=".form-control-lg"
-                            class="form-control form-control-lg">
-                    </div>
+                    <a href="#" type="submit" class="btn btn-primary">Simpan</a>
                 </form>
             </div>
         </div>
